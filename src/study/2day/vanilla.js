56c2eb6736231823e45afa55aca5351e7fe75cec
anko-developer <mikkeller@nate.com>
2020-02-16 19:43:10 +0900

    scope

--- src/study/2day/vanilla.js
+++ src/study/2day/vanilla.js
@@ -31,3 +31,15 @@ console.log(user.name);
  * 재할당이 필요한 경우에 한정해 let 키워드를 사용한다. 이때 변수의 스코프는 최대한 좁게 만든다.
  * 변경이 발생하지 않는(재할당이 필요 없는 상수) 원시 값과 객체에는 const 키워드를 사용한다. const 키워드는 재할당을 금지하므로 var, let 보다 안전하다.
  */
+
+
+(function(){
+    var MYAPP = {};
+    MYAPP.me = {
+        name: 'wook',
+        age: 33
+    };
+    console.log('test1: ' + MYAPP.me.name);
+}());
+
+ console.log('test2: ' + MYAPP.me.name);
