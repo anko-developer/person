ee890a71a7d356f3a836c94b0f6fbb31b34b40c0
anko-developer <mikkeller@nate.com>
2020-03-22 19:51:39 +0900

    Array

--- src/study/8day/vanilla.js
+++ src/study/8day/vanilla.js
@@ -33,4 +33,43 @@
     console.log(arr);
     
     
+    arr.forEach((item, index) => {
+        console.log(item, index);
+    });
+
+    for(const item of arr) {
+        console.log(item);
+    };
+
+
+    const test = Array.of('반', '려', '견');
+    if(test.indexOf('견') !== -1) {
+        console.log('일치하는 문자가 있군요.');
+    }
+    
+    if(test.includes('견')) {
+        console.log('일치하는 문자가 있군요.'); 
+    }
+
+    const str = ['d', 'o', 'g'];
+    console.log(str.reverse());
+    console.log(str.join('/'));
+    
+
+
+
+    const todos = [
+        { id: 1, content: 'HTML', completed: true },
+        { id: 2, content: 'CSS', completed: true },
+        { id: 3, content: 'Javascript', completed: false }
+    ];
+
+    const clone = todos.slice();
+    //const clone = [...todos];
+    console.log(clone === todos); // false
+
+    // 배열의 요소는 같다, 즉 얕은 복사가 된 것이다.
+    // Spread 문법과 Object.assign는 원본을 shallow copy한다.
+    // Deep copy를 위해서는 lodash의 deepClone을 사용하는 것을 추천
+    console.log(clone[0] === todos[0]); // true
 }());
