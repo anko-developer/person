d80b3c7ef6a90d905cac65f5bb41d4eec39fe2e0
anko-developer <mikkeller@nate.com>
2020-04-14 22:35:30 +0900

    vue..

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import './assets/stylesheets/styles.scss'; //sass 연결
-import './study/8day/vanilla.js';
+// import './study/8day/vanilla.js';
 
 import $ from 'jquery';
 const DOM = {
     body: '#Body'
 };
 
-$(DOM.body).append('<p>Webpack</p>');
+$(DOM.body).append('<h1>Webpack/Vue</h1>');
 // $(DOM.body).addClass('__block');
 // $(DOM.body).css('background-color', '#ffd200');
